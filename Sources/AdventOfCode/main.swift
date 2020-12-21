b90f48c6bb625c9fc3464737483ad09f078845b9
Sabino Paulicelli <sabino.paulicelli@mac.com>
2020-12-21 12:44:52 +0100

    Day 2 complete

--- Sources/AdventOfCode/main.swift
+++ Sources/AdventOfCode/main.swift
@@ -2,12 +2,17 @@
 import Day1
 import Day2
 
+print("Advent od Code 2020, Day 1:\n")
 if let day1 = Day1() {
     day1.printSolutionPart1()
     day1.printSolutionPart2()
 }
+print("---------------------------")
 
+print("Advent od Code 2020, Day 2:\n")
 if let day2 = Day2() {
     day2.printSolutionPart1()
-    // day2.printSolutionPart2()
+    day2.printSolutionPart2()
 }
+print("---------------------------")
+
--- Sources/Day2/Day2.swift
+++ Sources/Day2/Day2.swift
@@ -16,11 +16,68 @@ public struct Day2 {
     }
     
     public func printSolutionPart1() {
-        print(self.lines)
+        var validPasswords = 0
+        
+        // cycle through all lines
+        for line in self.lines {
+            
+            // use regex to split every line in components
+            if let atLeast = line.range(of: #"[0-9]*(?=-)"#,
+                                          options: .regularExpression),
+               let atLast = line.range(of: #"(?<=-)[0-9]*"#,
+                                       options: .regularExpression),
+               let char = line.range(of: #"[a-z](?=:)"#,
+                                     options: .regularExpression),
+               let password = line.range(of: #"(?<=: )[a-z]*"#,
+            options: .regularExpression) {
+                
+                // if a char is present, the password is split by the char in n parts - 1
+                let timesOccurred = line[password].components(separatedBy: line[char]).count - 1
+                if timesOccurred >= Int(line[atLeast])! && timesOccurred <= Int(line[atLast])! {
+                    validPasswords += 1
+                }
+                
+            }
+        }
+        
+        print("Found", validPasswords, "valid passwords")
     }
     
     public func printSolutionPart2() {
-
+        var validPasswords = 0
+        
+        // cycle through all lines
+        for line in lines {
+            
+            // use regex to split every line in components
+            if let position1 = line.range(of: #"[0-9]*(?=-)"#,
+                                          options: .regularExpression),
+               let position2 = line.range(of: #"(?<=-)[0-9]*"#,
+                                       options: .regularExpression),
+               let char = line.range(of: #"[a-z](?=:)"#,
+                                     options: .regularExpression),
+               let password = line.range(of: #"(?<=: )[a-z]*"#,
+            options: .regularExpression) {
+                
+                let passwordChars = Array(line[password])
+                let position1inPswd = Int(line[position1])! - 1 // as said in riddle, positions start at 1, not 0
+                let position2inPswd = Int(line[position2])! - 1
+                let char2Find = [Character](line[char])[0] // Substring converted in Character
+                                
+                // if a char is present at only one position, password is valid
+                if (
+                    (passwordChars[position1inPswd] == char2Find) ||
+                    (passwordChars[position2inPswd] == char2Find)
+                ) && !(
+                    (passwordChars[position1inPswd] == char2Find) &&
+                    (passwordChars[position2inPswd] == char2Find)
+                ) {
+                    validPasswords += 1
+                }
+            }
+        }
+        
+        print("Found", validPasswords, "valid passwords")
     }
     
 }
